b24e863cd01189a0599c96c8b8cbad746f7e3dd4
Adwaith <aj007boss@gmail.com>
2024-12-05 18:38:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'netflix-clone',
+  base: '/', // Ensure the base is the root path
   plugins: [react()],
-  
 })
